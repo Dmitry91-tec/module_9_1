7c01e95e52838d3229da43a5afb7a0f60398881b
Dmitry <nasdi91@yandex.ru>
2024-09-17 21:33:06 +1000

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,8 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for function_ in functions:
+        results[function_.__name__] = function_(int_list)
+    return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
